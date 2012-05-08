f35f62d141374277ac981ba6f41e160068c52064
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-05-08 17:18:25 -0400

    A new schema is available.

--- Software-Requirements-Specification/Schema.sql
+++ Software-Requirements-Specification/Schema.sql
@@ -33,6 +33,7 @@ create table TablePrefix_Member(
 
 
 create table TablePrefix_Bike(
+
 	id	integer auto_increment,
 		primary key(id),
 
@@ -43,33 +44,11 @@ create table TablePrefix_Bike(
 	serialNumber	varchar(80) not null,
 	acquisitionDate	date not null,
 
+	bikeSize   integer not null,
+
 	userIdentifier	integer not null,
 		index userIdentifier_index (userIdentifier),
 		foreign key (userIdentifier) references TablePrefix_User(id)
-
-) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci ;
-
-create table TablePrefix_Repair(
-	id	integer auto_increment,
-		primary key(id),
-
-	creationDate	datetime not null,
-
-
-	userIdentifier	integer not null,
-		index userIdentifier_index (userIdentifier),
-		foreign key (userIdentifier) references TablePrefix_User(id),
-
-	bikeIdentifier	integer not null,
-		index bikeIdentifier_index (bikeIdentifier),
-		foreign key (bikeIdentifier) references TablePrefix_Bike(id),
-
-	description	varchar(255) not null,
-
-	completionDate	datetime not null,
-
-	completionUserIdentifier	integer not null,
-		index completionUserIdentifier_index (completionUserIdentifier)
 
 ) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci ;
 
@@ -196,7 +175,7 @@ create table TablePrefix_MemberLock (
 	startingDate date not null,
 	endingDate date not null,
 
-	removed bool not null,
+	lifted bool not null,
 	explanation varchar(140) not null,
 
 	userIdentifier	integer not null,
@@ -215,12 +194,106 @@ create table TablePrefix_ClosedDay (
 		foreign key (placeIdentifier) references TablePrefix_Place(id),
 
 	dayOfYear date not null,
-	name varchar(100),
+	name varchar(100)
+
+) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci ;
+
+
+create table TablePrefix_Right(
+
+	id	integer auto_increment,
+		primary key(id),
+
+	placeIdentifier integer not null,
+		index placeIdentifier_index (placeIdentifier),
+		foreign key (placeIdentifier) references TablePrefix_Place(id),
+
+	userIdentifier	integer not null,
+		index userIdentifier_index (userIdentifier),
+		foreign key (userIdentifier) references TablePrefix_User(id),
+
+	rightNumber integer not null
+
+) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci ;
+
+create table TablePrefix_RepairType (
+
+	id	integer auto_increment,
+		primary key(id),
+
+	name varchar(100) unique
+
+) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci ;
+
+
+insert into TablePrefix_RepairType (name) values ('Autre');
+
+
+create table TablePrefix_Part (
+
+	id	integer auto_increment,
+		primary key(id),
+
+	name varchar(100) unique,
+
+	value double unsigned not null
+
+) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci ;
+
+
+create table TablePrefix_PartTransaction (
+
+	id	integer auto_increment,
+		primary key(id),
+
+	partIdentifier integer not null,
+		index partIdentifier_index (partIdentifier),
+		foreign key (partIdentifier) references TablePrefix_Part(id),
+
+	transactionDate datetime not null,
+
+	partChange double not null,
+
+	userIdentifier	integer not null,
+		index userIdentifier_index (userIdentifier),
+		foreign key (userIdentifier) references TablePrefix_User(id),
+
+	balance double not null
+
+
+) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci ;
+
+
+
+create table TablePrefix_Repair(
+	id	integer auto_increment,
+		primary key(id),
+
+	creationDate	datetime not null,
 
 
 	userIdentifier	integer not null,
 		index userIdentifier_index (userIdentifier),
-		foreign key (userIdentifier) references TablePrefix_User(id)
+		foreign key (userIdentifier) references TablePrefix_User(id),
+
+	bikeIdentifier	integer not null,
+		index bikeIdentifier_index (bikeIdentifier),
+		foreign key (bikeIdentifier) references TablePrefix_Bike(id),
+
+	repairTypeIdentifier	integer not null,
+		index repairTypeIdentifier_index (repairTypeIdentifier),
+		foreign key (repairTypeIdentifier) references TablePrefix_RepairType(id),
+
+	description	varchar(255) not null,
+
+	completionDate	datetime not null,
+
+	completionUserIdentifier	integer not null,
+		index completionUserIdentifier_index (completionUserIdentifier),
+		foreign key (completionUserIdentifier) references TablePrefix_User(id),
+
+	minutes int unsigned not null
+
 ) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci ;
 
 
