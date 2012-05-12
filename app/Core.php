b478151e8c0a40fc48e7c217493bc826cdd4304a
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-05-12 10:46:14 -0400

    This is version 1.2.2.

--- app/Core.php
+++ app/Core.php
@@ -166,7 +166,7 @@ class Core{
 		$CONFIG_style=$this->m_sheet;
 
 		$softwareName="cooprouelibre-StarFleetManager";
-		$softwareVersion="v1.2.1";
+		$softwareVersion="v1.2.2";
 
 		include("app/views/Template/template.php");
 	}
